c5185c81b3f44fa955f2ded184f0e5d2f97c1c7a
Cris Ryan Tan <crisryantan@gmail.com>
2023-04-21 11:42:22 +1000

    fetch then render

--- src/Jokes.tsx
+++ src/Jokes.tsx
@@ -2,10 +2,24 @@ import React, { FC, useEffect } from "react";
 import axios from "axios";
 
 const Jokes: FC<{ search: string }> = ({ search }) => {
+  const [jokes, setJokes] = React.useState([]);
+
+  useEffect(() => {
+    const request = {
+      method: 'GET',
+      url: `https://icanhazdadjoke.com/search?term=${search}&limit=10`,
+      headers: {
+        Accept: 'application/json',
+      }
+    };
+
+    axios(request).then((response) => setJokes(response.data.results));
+  }, [search]);
+
 
   return (
     <div className="jokes-container">
-      {[].map((j: any) => (
+      {jokes.map((j: any) => (
         <div className="joke" key={j.id}>
           {j.joke}
         </div>
