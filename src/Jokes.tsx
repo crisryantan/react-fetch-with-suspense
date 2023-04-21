88807b1ceec151429e3f5ca666ce2ee6647f974a
Cris Ryan Tan <crisryantan@gmail.com>
2023-04-21 11:48:38 +1000

    fetch then render

--- src/Jokes.tsx
+++ src/Jokes.tsx
@@ -1,21 +1,24 @@
 import React, { FC, useEffect } from "react";
 import axios from "axios";
 
+const request = {
+  method: 'GET',
+  url: `https://icanhazdadjoke.com/search?term=${''}&limit=10`,
+  headers: {
+    Accept: 'application/json',
+  }
+};
+
+const promise = axios(request)
+
 const Jokes: FC<{ search: string }> = ({ search }) => {
   const [jokes, setJokes] = React.useState([]);
 
   useEffect(() => {
-    const request = {
-      method: 'GET',
-      url: `https://icanhazdadjoke.com/search?term=${search}&limit=10`,
-      headers: {
-        Accept: 'application/json',
-      }
-    };
-
-    axios(request).then((response) => setJokes(response.data.results));
+    promise.then((response) => {
+      setJokes(response.data.results);
+    });
   }, [search]);
-
 
   return (
     <div className="jokes-container">
